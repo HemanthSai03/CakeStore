f1e8a3629c7b3d7e06d87167afbc9e5b1686339f
Hemanth Sai <hemanthsai0119@gmail.com>
2024-08-12 14:37:10 +0530

    Cake store

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
+import DocTitleOne from "./hooks/DocTitleOne";
+import DocTitleTwo from "./hooks/DocTitleTwo";
+import CounterTwo from "./hooks/CounterTwo";
+import CounterOne from "./hooks/CounterOne";
 //npm i react-redux
 function App() {
   return (
-    <Provider store={store}>
+    // <Provider store={store}>
       <div className="App">
-        <CakeContainer />
-        <HooksCakeContainer />
+        {/* <CakeContainer />
+        <HooksCakeContainer /> */}
+        <CounterOne/>
+        <CounterTwo/>
       </div>
-    </Provider>
+    // </Provider>
   );
 }
 
